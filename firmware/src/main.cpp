9b7503e58452664b95bb9bc471926408c11ec8d6
Ocean Bhatnagar <146022706+OceanBhatnagar@users.noreply.github.com>
2024-11-23 18:25:36 +0530

    Update main.cpp (#3)
    Updated the code of MPU6050 and Removed the code of GPS Module

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,6 +1,14 @@
 #include <WiFi.h>
+
 WiFiClient client;
 WiFiServer server(80);
+
+#include "Wire.h"
+#include <math.h>
+
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+Adafruit_MPU6050 mpu;
 
 //DHT11 Temperature and Humidity
 #include <DHT.h>
@@ -15,22 +23,6 @@ int h,t,p,alt;
 #include  <Adafruit_BMP280.h>
 Adafruit_BMP280 bmp; // I2C Interface
 
-//MPU6050
-#include <Adafruit_MPU6050.h>
-#include <Adafruit_Sensor.h>
-Adafruit_MPU6050 mpu;
-
-
-
-// Constants for Calculations
-  #define METABOLIC_EFFICIENCY 0.2  // Efficiency for cycling (20%)
-  #define TIME_STEP 0.1             // Sampling time step in seconds (100 ms)
-  #define PEDAL_ARM_LENGTH 0.175    // Pedal arm length in meters
- // Global Variables
- float totalEnergy = 0.0; // Accumulated energy in joules
-float caloriesBurned = 0;
-unsigned long previousTime = 0;
-
 //HX711+LOAD CELL
 #include "HX711.h"
 // HX711 circuit wiring
@@ -38,23 +30,34 @@ const int LOADCELL_DOUT_PIN = 12;
 const int LOADCELL_SCK_PIN = 14;
 HX711 scale;
 
-//GPS MODULE
-#include <TinyGPS++.h>
-static const int RXPin = 35, TXPin = 34;
-static const uint32_t GPSBaud = 9600;
-// The TinyGPS++ object
-TinyGPSPlus gps;
-#include <SoftwareSerial.h>
-// The serial connection to the GPS device
-SoftwareSerial ss(RXPin, TXPin);
-double previousLat = 0.0;
-double previousLng = 0.0;
-double totalDistance = 0.0;
+#define METABOLIC_EFFICIENCY 0.2  // Efficiency for cycling (20%)
+#define TIME_STEP 0.1             // Sampling time step in seconds (100 ms)
 
-void setup() 
-{
-  Serial.begin(9600);
-  WiFi.begin("Ocean134", "ocean123");
+// Global Variables
+float totalEnergy = 0.0; // Accumulated energy in joules
+float caloriesBurned = 0;
+float Winkel_neu, Winkel_alt;     // Current and previous inclination angles
+float gz_alt, gz_neu;             // Current and previous angular velocities
+float time_previous, time_current;         // Current and previous time
+float t_Start;          // Start time of a rotation
+float t_End;           // End time of a rotation
+float t_FUllROTATION;                // Time for a full rotation
+float v;                          // Average pedal speed
+float r=0.175;                          // Crank length
+int n;                            // Number of force measurements during one rotation
+float F;                          // Average force during a rotation
+float k=292;                          // Calibration factor specifying which numerical value [0, 1023] corresponds to which force in Newtons
+int offset;                       // Offset of the input voltage without any load, i.e., when F = 0
+float P;                          // Average power P
+ 
+
+
+void setup()
+   {
+    // Initialize serial communication
+    Serial.begin(115200);
+
+    WiFi.begin("Ocean134", "ocean123");
   while(WiFi.status() != WL_CONNECTED)
   {
     delay(200);
@@ -64,7 +67,8 @@ void setup()
   Serial.println("NodeMCU is connected!");
   Serial.println(WiFi.localIP());
   server.begin();
-  
+  //DHT
+  dht.begin();
   //BMP
   bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
                   Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
@@ -72,42 +76,33 @@ void setup()
                   Adafruit_BMP280::FILTER_X16,      /* Filtering. */
                   Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
 
-  //MPU6050
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  // set gyro range to +- 500 deg/s
-	mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  // set filter bandwidth to 21 Hz
-	mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-  
- // Initialize the HX711
-  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-  scale.set_scale(230); // Replace 230 with your calibrated scale factor
-  scale.tare();         // Reset the scale to 0
-  Serial.println("Scale is ready. Tared."); 
+    // =====================================================================================
+    // Read raw accelerometer/gyro measurements from the device
+    // Accelerometer output values: Resolution 2g: 16384/g, Resolution 4g: 8192/g
+    // Gyroscope output values: Resolution 250°/s: 131/°/s; 500°/s: 65.5/°/s; 2000°/s: 16.375/°/s
+    // =====================================================================================
+    //HX11
+    scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+    if (!scale.is_ready()) {
+    Serial.println("HX711 not ready. Check wiring.");
+    delay(500);
+    return;
+    }
+     //MPU6050
+    mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+	  mpu.setGyroRange(MPU6050_RANGE_500_DEG);// set gyro range to +- 500 deg/s
+	  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);// set filter bandwidth to 21 Hz
+    // Verify connection
+    // Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");  
+    time_previous = 0.0;
+    Winkel_alt = 0.0;
+    gz_alt = 0.0;
+   }
 
-  //GPS MODULE
-  ss.begin(GPSBaud);      
-  } 
 
-// Function to calculate distance between two points using Haversine formula
-double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
-  const double R = 6371000; // Radius of Earth in meters
-  double dLat = radians(lat2 - lat1);
-  double dLon = radians(lon2 - lon1);
 
-  lat1 = radians(lat1);
-  lat2 = radians(lat2);
-
-  double a = sin(dLat / 2) * sin(dLat / 2) +
-             cos(lat1) * cos(lat2) *
-             sin(dLon / 2) * sin(dLon / 2);
-  double c = 2 * atan2(sqrt(a), sqrt(1 - a));
-  return R * c; // Distance in meters
-  }
-  
-void loop() 
-{
-  //Temperature and altitude
+void loop(){
+    //Temperature and Humidity
    h=dht.readHumidity();
    t=dht.readTemperature();
 
@@ -126,106 +121,75 @@ void loop()
 
    Serial.print("Altitude:");
    Serial.print(alt);
-   Serial.println("  m");   
+   Serial.println("  m");  
+  
 
-  //LOAD CELL
-  float reading = scale.get_units(10); // Average 10 readings for better stability
-  if (!scale.is_ready()) {
-    Serial.println("HX711 not ready. Check wiring.");
-    delay(500);
-    return;
-  }
+   //HX11 AND MPU6050
+   float reading = scale.get_units(10); // Average 10 readings for better stability
+    n = 0;  // Reset the counter variable to 0
+    F = 0;  // Reset the average force to 0 
+    t_Start = micros();     // Determine the start time of a rotation
+    do {
+         sensors_event_t a, g, temp;
+        mpu.getEvent(&a, &g, &temp);
+        gz_neu = -g.gyro.z;   // New angular velocity
+    
+        // Calculate the new angle using the old angle and the average of the old and new angular velocities:
+        // ==============================================================================================================
+    
+        time_current = micros();    // Current runtime of the program in µs
+    
+        Winkel_neu = Winkel_alt + 0.5 * ((gz_alt + gz_neu) / 65.5) * ((time_current - time_previous) / 1000000.0);
+        
+        if (Winkel_neu < 0)   // Restart at negative angle
+           {
+            Winkel_neu = 0.0;
+            t_Start = micros();
+            n = 0;  // Reset counter variable
+            F = 0;  // Reset the average force
+           }
+    
+        F = F + k * (5.0 * (reading) / 1023.0);  // Integrate all forces acting during one rotation; Factor k = Newton/Volt!
 
-   //MPU6050 
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
+        gz_alt = gz_neu;
+        time_previous = time_current;
+        Winkel_alt = Winkel_neu;
+        n = n + 1;               // Increment the counter variable
+        
+       }
+    while (Winkel_neu < 360);    // Exit the loop when the angle > 360°, i.e., after a full rotation
+    // ============================
+    // == Complete Rotation ==    
+    // ============================
+    t_End = micros();        // Determine the end time of a rotation
+    Winkel_alt = Winkel_alt - 360.0;    // Reduce the angle to the interval [0, 360]
+    F = F / n;                          // Calculate the average force during one rotation
+    t_FUllROTATION = (t_End - t_Start) / 1000000.0;    // Calculate the rotation time in seconds
+    v = 2.0 * r * 3.141592654 / t_FUllROTATION;    // Calculate the average pedal speed
+    P = 2.0 * F * v;                            // Calculate the average power P
+    int RPM = 60.0 / t_FUllROTATION;
+    float energy = P * TIME_STEP; // Joules
+    totalEnergy += energy;
+    float caloriesBurned = METABOLIC_EFFICIENCY * (totalEnergy / 4184.0);
+    // Print data for debugging
+    Serial.print("RPM: ");
+    Serial.print(RPM);
+    Serial.print(" Power: ");
+    Serial.print(P);
+    Serial.print("W Calories Burned: ");
+    Serial.println(caloriesBurned);
 
-	/* Print out the values */
-	Serial.print("Acceleration X: ");
-	Serial.print(a.acceleration.x);
-	Serial.print(", Y: ");
-	Serial.print(a.acceleration.y);
-	Serial.print(", Z: ");
-	Serial.print(a.acceleration.z);
-	Serial.println(" m/s^2");
-
-	Serial.print("Rotation X: ");
-	Serial.print(g.gyro.x);
-	Serial.print(", Y: ");
-	Serial.print(g.gyro.y);
-	Serial.print(", Z: ");
-	Serial.print(g.gyro.z);
-	Serial.println(" rad/s"); 
-
-   // Acceleration and Angular Velocity
-  float acceleration = sqrt(pow(a.acceleration.x, 2) + pow(a.acceleration.y, 2) + pow(a.acceleration.z, 2));
-  float angularVelocity = sqrt(pow(g.gyro.x, 2) + pow(g.gyro.y, 2) + pow(g.gyro.z, 2));
-
-  // Calculate RPM
-  int rpm = (angularVelocity * 60.0) / (2.0 * M_PI);
-
-  // Calculate Torque
-  float torque = reading * PEDAL_ARM_LENGTH;
-
-  // Calculate Power
-  float Power = torque * angularVelocity;
-
-  // CalculateEnergy and Calories Burned
-  float energy = Power * TIME_STEP; // Joules
-  totalEnergy += energy;
-  float caloriesBurned = METABOLIC_EFFICIENCY * (totalEnergy / 4184.0);
-
-  // Display the results
-  Serial.print("RPM: ");
-  Serial.println(rpm);
-
-  Serial.print("Torque (Nm): ");
-  Serial.println(torque);
-
-  Serial.print("Power (Watts): ");
-  Serial.println(Power);
-
-  Serial.print("Calories Burned: ");
-  Serial.println(caloriesBurned); 
-
-
-
-  //GPS MODULE
-    while (ss.available() > 0) {
-    gps.encode(ss.read());
-
-    if (gps.location.isUpdated()) {
-      double currentLat = gps.location.lat();
-      double currentLng = gps.location.lng();
-
-      // Check if there is a valid previous point to calculate distance
-      if (previousLat != 0.0 && previousLng != 0.0) {
-        double distance = calculateDistance(previousLat, previousLng, currentLat, currentLng);
-        totalDistance += distance; // Accumulate total distance
-      }
-
-      // Update previous coordinates
-      previousLat = currentLat;
-      previousLng = currentLng;
-
-      // Print GPS coordinates and total distance
-      Serial.print("Latitude= ");
-      Serial.print(currentLat, 6);
-      Serial.print(" Longitude= ");
-      Serial.print(currentLng, 6);
-      Serial.print(" Distance Travelled= ");
-      Serial.print(totalDistance, 2); // Print distance in meters
-      Serial.println(" m");
-    }
-  }
-  //WEBSITE
+   
+   
+    //WEBSITE
    WiFiClient client = server.available();
-  if (client) {
+   if (client) {
     Serial.println("New Client Connected");
     // Wait until the client sends a request
     while (client.connected() && !client.available()) {
       delay(1);
     }
+   
 
     // Read and ignore the client request (we are not parsing it here)
     client.readStringUntil('\r');
@@ -244,8 +208,6 @@ void loop()
 
     // Display welcome message and sensor data
     client.println("<h1>Welcome to the MPU6050 Sensor Data Page</h1>");
-    client.println("<h3>LED Controls</h3>");
-    client.println("<br>");
     client.println("<p><b>TEMPERATURE AND HUMIDITY Data:</b></p>");
     client.println("<p>" + String(h) + "</p>");
     client.println("<p>" + String(t) + "</p>");
@@ -253,21 +215,20 @@ void loop()
     client.println("<p>" + String(p) + "</p>"); 
     client.println("<p>" + String(alt) + "</p>");
     client.println("<p><b>IMU DATA:</b></p>");
-    client.println("<p>" + String(rpm)+ "</p>");
-    client.println("<p>" + String(torque) + "</p>");
-    client.println("<p>" + String(Power) + "</p>");
+    client.println("<p>" + String(RPM)+ "</p>");
+    client.println("<p>" + String(P) + "</p>");
     client.println("<p>" + String(caloriesBurned) + "</p>");
     client.println("<p><b>FORCE APPLIED ON PEDAL:</b></p>");
-    long reading = scale.get_units(10);
     client.println("<p>Current Scale Reading: " + String(reading) + " grams</p>");
-    client.println("<p><b>DISTANCE TRAVELLED:</b></p>"); //totalDistance
-    client.println("<p>" + String(totalDistance) + "</p>");
     
 
     // End of HTML document
     client.println("</body>");
     client.println("</html>");
+    // Close the connection
+    client.stop();
   }
-}
+}  
+
 
 
