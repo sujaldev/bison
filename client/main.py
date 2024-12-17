5de21097ad065629b04c0f2120e8640945913edc
Sujal Singh <email.sujalsingh@gmail.com>
2024-12-17 08:18:51 +0530

    add force plot

--- client/main.py
+++ client/main.py
@@ -20,21 +20,30 @@ def read_values(io: Serial = serial) -> list[float]:
     return [float(item) for item in io.readline().decode().split()]
 
 
+for _ in range(10):
+    read_values()
+
 # Setup initial data
 try:
     z_buffer = deque([read_values()[2]] * MAX_BUFFER, maxlen=MAX_BUFFER)  # Raw z data (with noise filter).
     z_avg_buffer = z_buffer.copy()  # Simple moving average of `z_buffer`.
+    force_buffer = deque([read_values()[-1]] * MAX_BUFFER, maxlen=MAX_BUFFER)
+    force_avg_buffer = force_buffer.copy()
 except ValueError:
     z_buffer = deque([0] * MAX_BUFFER, maxlen=MAX_BUFFER)
     z_avg_buffer = z_buffer.copy()
+    force_buffer = deque([0] * MAX_BUFFER, maxlen=MAX_BUFFER)
+    force_avg_buffer = force_buffer.copy()
 time_buffer = deque([time()] * MAX_BUFFER, maxlen=MAX_BUFFER)  # Timestamps for items in z_buffer.
 slope_buffer = deque([0.0] * MAX_BUFFER, maxlen=MAX_BUFFER)  # Slope for items in z_avg_buffer.
 intercept_time_buffer = deque([0.0] * 3, maxlen=3)  # Timestamps at which an x-intercept occurred.
 rpm = 0
+force = 0
 
 # Setup plot
-fig, ax1 = plt.subplots()
-text = fig.text(.5, .5, f"RPM: {rpm}", fontsize=15)
+fig, (ax1, ax2) = plt.subplots(2)
+RPM_LABEL = fig.text(.15, .85, f"RPM: {rpm}", fontsize=15)
+LOAD_LABEL = fig.text(.15, .4, f"Force (g): {force}", fontsize=15)
 
 
 def animate(_interval):
@@ -44,23 +53,27 @@ def animate(_interval):
 
 def process_data():
     global rpm
+    global force
 
     current_time = time()
 
     try:
-        a_x, a_y, a_z, r_x, r_y, r_z = read_values(serial)
+        a_x, a_y, a_z, load = read_values(serial)
     except ValueError:
         # rotating at high speeds probably causes loose connections and therefore errors reading from the sensor.
         # (which should be fixed when we solder it all)
         append_z_value(z_buffer[-1], current_time)
+        append_load_value(force_buffer[-1])
         return
 
     # Noise filter
     if abs(a_z - z_buffer[-1]) < NOISE_THRESHOLD:
         append_z_value(z_buffer[-1], current_time)
+        append_load_value(load)
         return
 
     append_z_value(a_z, current_time)
+    append_load_value(load)
 
     # An x-intercept occurred
     if not (slope_buffer[-1] * slope_buffer[-2] < 0):
@@ -91,10 +104,13 @@ def process_data():
 
 def update_plot():
     ax1.clear()
+    ax2.clear()
     ax1.plot(time_buffer, z_buffer, label="raw", linewidth=1)
+    ax2.plot(time_buffer, force_buffer, label="raw")
     # ax1.plot(time_buffer, z_avg_buffer, label="avg")
     # ax1.plot(time_buffer, slope_buffer, label="slope")
-    text.set_text(f"RPM: {rpm}")
+    RPM_LABEL.set_text(f"RPM: {rpm}")
+    LOAD_LABEL.set_text(f"Force(g): {force}")
 
 
 def append_z_value(val: float, t: float):
@@ -102,6 +118,13 @@ def append_z_value(val: float, t: float):
     time_buffer.append(t)
     z_avg_buffer.append(moving_average(z_buffer))
     slope_buffer.append((z_avg_buffer[-1] - z_avg_buffer[-2]) / (time_buffer[-1] - time_buffer[-2]))
+
+
+def append_load_value(val: float):
+    global force
+    force = val
+    force_buffer.append(val)
+    force_avg_buffer.append(moving_average(force_buffer))
 
 
 def moving_average(data: deque[float], window=2) -> float:
